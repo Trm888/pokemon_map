024884759dd54c0d734da10bd9b0df60ade6e3e1
Trm888 <bigtram888@gmail.com>
2022-10-14 00:12:18 +0600

    Remove unnecessary conversion

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -8,7 +8,7 @@ class Pokemon(models.Model):
     title_jp = models.CharField(max_length=200, blank=True, verbose_name='Название на японском')
     evolution_from = models.ForeignKey('self', null=True, on_delete=models.SET_NULL, blank=True, related_name='evolutions', verbose_name='Эволюция')
     def __str__(self):
-        return f'{self.title}'
+        return f'{self}'
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
